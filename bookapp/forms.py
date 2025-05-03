6b60cdfc09824475e847b00a0f7c28bf35b8769c
DISHAV16 <vdisha421@gmail.com>
2025-05-03 13:56:58 +0530

    Add models for reading tracker

--- bookapp/forms.py
+++ bookapp/forms.py
@@ -1,85 +1,40 @@
 from django import forms
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Book, User
 from django.core.exceptions import ValidationError
 
-from bookapp.models import Book
-
-ERROR_NO_INPUT = "This field is required."
-ERROR_DUPLICATE_BOOKS = "This book already exists on your list."
-ERROR_DUPLICATE_USERS = "A user with that username already exists."
-
-
-class BookForm(ModelForm):
-    """Form for creating books."""
+class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ["title", "current_page", "total_pages", "user", "all_books"]
-        widgets = {
-            "title": forms.fields.TextInput(attrs={
-                "placeholder": "Book Title",
-                "class": "form-control input-lg",
-            }),
-            "current_page": forms.fields.NumberInput(attrs={
-                "placeholder": "Current page",
-                "class": "form-control input-lg",
-            }),
-            "total_pages": forms.fields.NumberInput(attrs={
-                "placeholder": "Total pages",
-                "class": "form-control input-lg",
-            }),
-        }
-        error_messages = {
-            "title": {"required": ERROR_NO_INPUT,
-                      "unique_together": ERROR_DUPLICATE_BOOKS},
-            "current_page": {"required": ERROR_NO_INPUT},
-            "total_pages": {"required": ERROR_NO_INPUT},
-        }
+        fields = ['title', 'author', 'genre', 'status']
 
-    def save(self, for_list):
-        self.instance.all_books = for_list
-        return super().save()
-
-
-class BooksInList(BookForm):
-    """Return existing books in list."""
-
-    def __init__(self, for_list, user, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)  # Get user from kwargs
         super().__init__(*args, **kwargs)
-        self.instance.all_books = for_list
-        self.user = user
 
-    def validate_unique(self):
-        """Check for duplicate book titles in list."""
-        try:
-            self.instance.validate_unique()
-        except ValidationError as e:
-            e.error_dict = {"title": [ERROR_DUPLICATE_BOOKS]}
-            self._update_errors(e)
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if Book.objects.filter(title=title, user=self.user).exists():
+            raise ValidationError("You have already added a book with this title.")
+        return title
 
-    def save(self):
-        return ModelForm.save(self)
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.user = self.user  # Assign the user before saving
+        if commit:
+            instance.save()
+        return instance
 
 
-class UserForm(UserCreationForm):
-    """Form for creating new users."""
-    first_name = forms.CharField(
-        label="First name", required=True, widget=forms.TextInput)
-    email = forms.EmailField(label="Email", widget=forms.EmailInput)
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ("username", "first_name", "email", "password1", "password2")
-        help_texts = {
-            "username": None,
-            "password": None,
-        }
+        fields = ['username', 'email', 'password', 'is_profile_public']
 
-    def validate_unique(self):
-        """Check for duplicate usernames."""
-        try:
-            self.instance.validate_unique()
-        except ValidationError as e:
-            e.error_dict = {"username": [ERROR_DUPLICATE_USERS]}
-            self._update_errors(e)
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.password_hash = self.cleaned_data['password']  # Simple store, not hashed
+        if commit:
+            user.save()
+        return user
--- bookapp/views.py
+++ bookapp/views.py
@@ -1,152 +1,88 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from bookapp.models import Book, AllBooks
-from bookapp.forms import BookForm, BooksInList, UserForm, ERROR_DUPLICATE_BOOKS
+from bookapp.models import Book
+from bookapp.forms import BookForm, UserForm
 
 
+@login_required
 def new_book_form(request):
-    return render(request, "bookForm.html", {"form": BookForm()})
-
-
-@login_required
-def view_list(request, all_books_id):
-    """View for displaying book list."""
-    labels = []
-    data = []
-
-    all_books = AllBooks.objects.get(id=all_books_id)
-    all_books_progess_chart = Book.objects.filter(all_books=all_books)
-
-    for book in all_books_progess_chart:
-        labels.append(book.title)
-        percentage = round(book.current_page / book.total_pages * 100, 2)
-        data.append(percentage)
-
-    form = BooksInList(for_list=all_books, user=request.user)
-    if request.method == "POST":
-        form = BooksInList(for_list=all_books,
-                           user=request.user, data=request.POST)
-        if form.is_valid():
-            form = None
-            form = BookForm(data=request.POST)
-            form = form.save(for_list=all_books)
-            form.user = request.user
-            form.save()
-            return redirect(all_books)
-
-    return render(request, "list.html", {"labels": labels,
-                                         "data": data,
-                                         "all_books": all_books,
-                                         "form": form,
-                                         })
-
-
-@login_required
-def new_list(request):
-    """View for new user's book list."""
-    form = BookForm(data=request.POST)
-    if form.is_valid():
-        all_books = AllBooks.objects.create()
-        form = form.save(for_list=all_books)
-        form.user = request.user
-        form.all_books = all_books
-        form = form.save()
-
-        return redirect(all_books)
-
-    else:
-        return render(request, "bookForm.html", {"form": form})
+    """Render form for adding a new book."""
+    return render(request, "bookForm.html", {"form": BookForm(user=request.user)})
 
 
 @login_required
 def my_list(request):
-    """View for manage book list form."""
-    all_books_progess_chart = Book.objects.filter(user=request.user)
+    """Display and manage books added by the logged-in user."""
+    user_books = Book.objects.filter(user=request.user)
+    labels = [book.title for book in user_books]
+    data = [100 if book.status == "Completed" else 0 for book in user_books]  # Example progress logic
 
-    book_set_id = []
-    all_books_set_id = []
-    for i in all_books_progess_chart:
-        book_set_id.append(i.id)
-        all_books_set_id.append(i.all_books_id)
-
-    labels = []
-    data = []
-
-    if all_books_set_id != []:
-        for book in all_books_progess_chart:
-            labels.append(book.title)
-            percentage = round(book.current_page / book.total_pages * 100, 2)
-            data.append(percentage)
-
-        all_books = AllBooks.objects.get(id=all_books_set_id[0])
-        form = BooksInList(for_list=all_books, user=request.user)
-
-        if request.method == "POST":
-            form = BooksInList(for_list=all_books,
-                               user=request.user, data=request.POST)
-            if form.is_valid():
-                form = None
-                form = BookForm(data=request.POST)
-                form = form.save(for_list=all_books)
-                form.user = request.user
-                form.save()
-                return redirect(all_books)
-
-            else:
-                messages.error(request, ERROR_DUPLICATE_BOOKS)
-
-        form = BooksInList(for_list=all_books, user=request.user)
-
-        return render(request, "myList.html", {"labels": labels,
-                                               "data": data,
-                                               "all_books": all_books,
-                                               "form": form,
-                                               })
-
+    if request.method == "POST":
+        form = BookForm(request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("my_list")
     else:
-        return render(request, "bookForm.html", {"form": BookForm()})
+        form = BookForm(user=request.user)
+
+    return render(request, "myList.html", {
+        "labels": labels,
+        "data": data,
+        "form": form,
+        "books": user_books
+    })
+
+
+@login_required
+def delete_book(request, pk):
+    """Delete a specific book owned by the current user."""
+    book = get_object_or_404(Book, pk=pk, user=request.user)
+    if request.method == "POST":
+        book.delete()
+        return redirect("my_list")
+
+    return render(request, "confirm_delete.html", {"book": book})
 
 
 def register(request):
-    """Register new user to database."""
+    """Register a new user."""
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            return render(request, 'registered.html', {'user': user})
+            user = form.save()
+            login(request, user)
+            return redirect("my_list")
     else:
         form = UserForm()
     return render(request, 'register.html', {'form': form})
 
+def view_list(request, list_id):
+    """View to display books in a specific list (could be by genre or category)."""
+    books = Book.objects.filter(user=request.user, list_id=list_id)  # Assuming list_id is a field in Book
+    return render(request, 'view_list.html', {'books': books})
 
 def login_view(request):
-    """Log in user and redirect to home page."""
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user:
-        login(request, user)
-        return render(request, "/")
-        ...
-    else:
-        messages.error(request, "Invalid login")
+    """Log in user and redirect to book list."""
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        # Manual authentication for custom user model
+        from bookapp.models import User
+        try:
+            user = User.objects.get(username=username, password_hash=password)
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect("my_list")
+        except User.DoesNotExist:
+            messages.error(request, "Invalid login")
+    
+    return render(request, "login.html")
 
 
 def logout_view(request):
-    """Log out user and redirect to login pagel."""
+    """Log out user."""
     logout(request)
-    return redirect(request, "/login")
-
-
-def delete_book(request, pk):
-    """Delete book from list."""
-    book = Book.objects.get(pk=pk)
-    if request.method == "POST":
-        book.delete()
-        return redirect("/")
-
-    return render(request, "myList.html", {"book": book})
+    return redirect("login")
